4ef84459715ec09f0c45dc4c85801595c4710853
Akinola Femi <akinolafemi573@gmail.com>
2025-08-23 21:09:08 +0100

    Fix: Toothpaste category typo

--- client/src/app/Components/Category/Categories.jsx
+++ client/src/app/Components/Category/Categories.jsx
@@ -118,7 +118,7 @@ const Categories = () => {
         {/* Filter Buttons */}
         <div className="max-sm:px-0 p-3">
           <div className="grid max-sm:w-[5rem] max-sm:h-[20rem] h-[30rem] ml-3 w-[10rem] max-sm:grid-cols-1 my-3 gap-4">
-            {['All', 'Viva Plus', 'Viva Bar', 'Liquid Soap', 'Toothpase', 'Rafa'].map((cat) => (
+            {['All', 'Viva Plus', 'Viva Bar', 'Liquid Soap', 'Toothpaste', 'Rafa'].map((cat) => (
               <button
                 key={cat}
                 onClick={() => handleFilter(cat)}
