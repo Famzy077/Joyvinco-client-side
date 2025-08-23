627580a69bea76a5fddd14476eb1e2dab69cf6b8
Akinola Femi <akinolafemi573@gmail.com>
2025-08-23 20:40:01 +0100

    Fix: Viva Bar category tag

--- src/app/Components/VivaBar.jsx
+++ src/app/Components/VivaBar.jsx
@@ -63,7 +63,7 @@ export const VivaBar = () => {
   });
 
   const mobileProducts = allProducts?.filter(
-    (product) => product.category === "vivaSanitizer" 
+    (product) => product.category === "Viva Bar" 
   );
   
 
